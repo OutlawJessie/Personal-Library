bc974baedb011e437a111fd116dcba5f142ed8db
Jessie <FranceJessieJ@gmail.com>
2020-09-19 15:12:12 -0600

    Added api responses for missing title, book id, and/or comment.

--- routes/api.js
+++ routes/api.js
@@ -34,31 +34,36 @@ module.exports = function (app) {
     
     /* POST a book title to /api/books  */
     .post(function (req, res, next){
-      
-      // Create a book object with the new title.
-      var book = new Book(
-          { title: req.body.title}
-      );
+      // Check if user gave a title.
+      if (!req.body.title){
+          res.send('missing title');
+      } else {
+          // Create a book object with the new title.
+          var book = new Book(
+              { title: req.body.title}
+          );
 
-      // Search to see if book already exists...
-      Book.findOne({'title': req.body.title})
-          .exec(function (err, foundBook){ 
-              if (err) {return next(err); }
+          // Search to see if book already exists...
+          Book.findOne({'title': req.body.title})
+              .exec(function (err, foundBook){ 
+                  if (err) {return next(err); }
 
-              // If the book exists, let the user know.
-              if (foundBook){
-                  res.send('title already exists');
-              } else {
-                  // The book doesn't exist, so save it.
-                  book.save(function(err) {
-                      if (err) {return next(err);}
+                  // If the book exists, let the user know.
+                  if (foundBook){
+                      res.send('title already exists');
+                  } else {
+                      // The book doesn't exist, so save it.
+                      book.save(function(err) {
+                          if (err) {return next(err);}
                       
-                      // Give the json response.
-                      res.json({'title': book.title, 'comments': book.comments, '_id': book._id});
-                  });              
+                          // Give the json response.
+                          res.json({'title': book.title, 'comments': book.comments, '_id': book._id});
+                      });              
 
-              }
-          }); 
+                    }
+                }); 
+          }
+
     })
     
     /* DELETE all books at /api/books/ */
@@ -86,24 +91,34 @@ module.exports = function (app) {
     /* POST a comment for a book id at /api/books */
     .post(function(req, res, next){
 
+      // Check if user gave an id.
+      if (!req.params.id){
+          res.send('missing book id');
+      } else {
+          // Check if user gave a comment.
+          if (!req.body.comment){
+              res.send('missing comment');
+          } else { // User gave an id and a comment....
 
-      // Find the book by id, and initiate callback function.
-      Book.findById({_id: req.params.id}, function(err, book){
-          if (err) {return next(err);}
-          // Check if a book was found.
-          if (!book){
-              res.send('no book exists'); // FCC repl example shows 'null' in their example.
-          } else {
-              // Push the comment to the book that was found.
-              book.comments.push(req.body.comment);
-
-              // Save the book, and deliver the json response.
-              book.save(function(err, savedBook){
+              // Find the book by id, and initiate callback function.
+              Book.findById({_id: req.params.id}, function(err, book){
                   if (err) {return next(err);}
-                  res.json({'_id': savedBook._id, 'title': savedBook.title, 'comments': savedBook.comments});
+                  // Check if a book was found.
+                  if (!book){
+                      res.send('no book exists'); // FCC repl example shows 'null' in their example.
+                  } else {
+                      // Push the comment to the book that was found.
+                      book.comments.push(req.body.comment);
+
+                      // Save the book, and deliver the json response.
+                      book.save(function(err, savedBook){
+                          if (err) {return next(err);}
+                          res.json({'_id': savedBook._id, 'title': savedBook.title, 'comments': savedBook.comments});
+                      });
+                  }
               });
           }
-      });
+      }
     })
     
     /* DELETE a book by its id at /api/books */
