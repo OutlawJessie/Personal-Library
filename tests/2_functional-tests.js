87f4358fbb68c8a028af92629cdcbbe8660c0eff
Jessie <FranceJessieJ@gmail.com>
2020-09-22 16:36:45 -0600

    Added 2 more test books and tested DELETE all books.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -25,7 +25,7 @@ suite('Functional Tests', function() {
   * ----[EXAMPLE TEST]----
   * Each test should completely test the response of the API end-point including response status code!
   */
-  test('#example Test GET /api/books', function(done){
+  /*test('#example Test GET /api/books', function(done){
      chai.request(server)
       .get('/api/books')
       .end(function(err, res){
@@ -36,7 +36,7 @@ suite('Functional Tests', function() {
         assert.property(res.body[0], '_id', 'Books in array should contain _id');
         done();
       });
-  });
+  });*/
   /*
   * ----[END of EXAMPLE TEST]----
   */
@@ -46,18 +46,45 @@ suite('Functional Tests', function() {
 
     suite('POST /api/books with title => create book object/expect book object', function() {
       
+      /* This tests populating the library with a book. The book id here is used in other tests. */
       test('Test POST /api/books with title', function(done) {
         chai.request(server)
             .post('/api/books')
             .send({title: 'No One Will Read This Book'})
             .end(function(err, res){
-                testDummy = res.body._id;       // Needs to be deleted in final test.
+                testDummy = res.body._id;       // Needs to be deleted in 2nd to last test.
                 assert.equal(res.status, 200);
                 assert.equal(res.body.title, 'No One Will Read This Book');
                 done();
             });
       });
-      
+
+      /* This tests populating the library with another book that will be deleted later. */
+      test('Test POST /api/books with title 2', function(done) {
+        chai.request(server)
+            .post('/api/books')
+            .send({title: 'No One Will Read This Book 2'})
+            .end(function(err, res){
+                assert.equal(res.status, 200);
+                assert.equal(res.body.title, 'No One Will Read This Book 2');
+                done();
+            });
+      });
+
+
+      /* This tests populating the library with yet another book that will be deleted later. */
+      test('Test POST /api/books with title 3', function(done) {
+        chai.request(server)
+            .post('/api/books')
+            .send({title: 'No One Will Read This Book 3'})
+            .end(function(err, res){
+                assert.equal(res.status, 200);
+                assert.equal(res.body.title, 'No One Will Read This Book 3');
+                done();
+            });
+      });
+	
+      /* This tests posting a book with no title given. */
       test('Test POST /api/books with no title given', function(done) {
           chai.request(server)
               .post('/api/books')
@@ -73,7 +100,8 @@ suite('Functional Tests', function() {
 
 
     suite('GET /api/books => array of books', function(){
-      
+
+      /* This tests getting a list of books in the library. */
       test('Test GET /api/books',  function(done){
           chai.request(server)
               .get('/api/books')
@@ -96,6 +124,7 @@ suite('Functional Tests', function() {
 
     suite('GET /api/books/[id] => book object with [id]', function(){
       
+      /* This tests getting an invalid book id. */
       test('Test GET /api/books/[id] with id not in db',  function(done){
           chai.request(server)
               .get('/api/books/7f666897a3a0719ad5da38ff')
@@ -105,10 +134,11 @@ suite('Functional Tests', function() {
                   done();
               });
       });
-      
+
+      /* This tests getting a book by a valid id that exists in the database. */
       test('Test GET /api/books/[id] with valid id in db',  function(done){
           chai.request(server)
-              .get('/api/books/5f666897a3a0714ad5da38ff')
+              .get('/api/books/' + testDummy)
               .end(function(err, res){
                   assert.equal(res.status, 200);
                   assert.isObject(res.body);
@@ -120,7 +150,8 @@ suite('Functional Tests', function() {
 
 
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
-      
+
+     /* This tests posting a comment for a valid id. */
       test('Test POST /api/books/[id] with comment', function(done){
           chai.request(server)
 	      .post('/api/books/' + testDummy)
@@ -140,8 +171,9 @@ suite('Functional Tests', function() {
 
 
 
-    suite('POST /api/books/ => add blank comment for book id/expect missing comment', function(){
-      
+    suite('POST /api/books/[id] => add blank comment for book id/expect missing comment', function(){
+
+      /* This tests posting a blank comment (i.e. no comment) for a valid book id. */
       test('Test POST /api/books/[id] without comment', function(done){
           chai.request(server)
 	      .post('/api/books/' + testDummy)
@@ -172,8 +204,24 @@ suite('Functional Tests', function() {
                     });
       });
       
-    }); 
+      }); 
 
+      /* This tests deleting all the books in the library. */
+      suite('DELETE /api/books/ => delete all books', function(){
+              
+            test('Test DELETE /api/books/[id]', function(done){
+                chai.request(server)
+	            .delete('/api/books/')
+	            .end(function(err, res){
+                        assert.equal(res.status, 200)
+                        assert.strictEqual(res.text, 'complete delete successful');
+                        done();
+                    });
+      });
+      
+      });
+
+      
 
 
   });
